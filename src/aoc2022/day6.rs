54b8e8d21943915764af15651d9598e55e5106e3
J.B.C <juanbicortelezzi@gmail.com>
2022-12-06 16:13:33 -0300

    aoc22 day 6

---
+++ src/aoc2022/day6.rs
@@ -0,0 +1,48 @@
+fn get_input() -> &'static str {
+    include_str!("./inputs/day6.real")
+    // "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+}
+
+// answer: 1702
+pub fn solve_one() {
+    let window_size = 4;
+    let chars = get_input()
+        .as_bytes()
+        .windows(window_size)
+        .position(|group| {
+            let mut bit_count = 0u32;
+            for c in group {
+                let copy = bit_count;
+                bit_count |= 1 << (c - b'a');
+                if copy == bit_count {
+                    return false;
+                }
+            }
+            true
+        })
+        .unwrap();
+
+    println!("result: {:?}", chars + window_size);
+}
+
+// answer: 3559
+pub fn solve_two() {
+    let window_size = 14;
+    let chars = get_input()
+        .as_bytes()
+        .windows(window_size)
+        .position(|group| {
+            let mut bit_count = 0u32;
+            for c in group {
+                let copy = bit_count;
+                bit_count |= 1 << (c - b'a');
+                if copy == bit_count {
+                    return false;
+                }
+            }
+            true
+        })
+        .unwrap();
+
+    println!("result: {:?}", chars + window_size);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use aoc::aoc2022::day5::solve_one;
-use aoc::aoc2022::day5::solve_two;
+use aoc::aoc2022::day6::solve_one;
+use aoc::aoc2022::day6::solve_two;
 fn main() {
     solve_one();
     // solve_two();
